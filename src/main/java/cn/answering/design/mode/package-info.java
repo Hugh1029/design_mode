b62ac69518dfd24876912c5d669709d3a28c4e3c
zhangjp6 <zhangjp6@asiainfo.com>
2020-04-14 22:57:17 +0800

    init

---
+++ src/main/java/cn/answering/design/mode/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhangjp
+ * @date 2020-04-14 22:56
+ * @qq 34948062
+ * @github: https://github.com/Hugh03
+ * @web: https://answering.cn
+ * @description
+ */
+package cn.answering.design.mode;
